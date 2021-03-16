7f9891cecaac64af92a033d5a7e803308bededce
ConsolePeasant92 <hugocornel@yahoo.com>
2021-03-16 13:44:05 +0100

    added more graphics script functionality

--- Development/Source/script_interpreter_func_fe3d_graphics.cpp
+++ Development/Source/script_interpreter_func_fe3d_graphics.cpp
@@ -7,7 +7,7 @@ bool ScriptInterpreter::_executeFe3dGraphicsFunction(const string& functionName,
 	{
 		if (_validateListValueAmount(arguments, 0) && _validateListValueTypes(arguments, {}))
 		{
-			_fe3d.gfx_enableFog(_fe3d.gfx_getFogMinDistance(), _fe3d.gfx_getFogMaxDistance(), 
+			_fe3d.gfx_enableFog(_fe3d.gfx_getFogMinDistance(), _fe3d.gfx_getFogMaxDistance(),
 				_fe3d.gfx_getFogThickness(), _fe3d.gfx_getFogColor());
 			returnValues.push_back(ScriptValue(_fe3d, ScriptValueType::EMPTY));
 		}
@@ -63,7 +63,7 @@ bool ScriptInterpreter::_executeFe3dGraphicsFunction(const string& functionName,
 		if (_validateListValueAmount(arguments, 0) && _validateListValueTypes(arguments, {}))
 		{
 			auto result = _fe3d.gfx_getFogMaxDistance();
-			returnValues.push_back(ScriptValue(_fe3d, ScriptValueType::DECIMAL));
+			returnValues.push_back(ScriptValue(_fe3d, ScriptValueType::DECIMAL, result));
 		}
 	}
 	else if (functionName == "fe3d:graphics_set_fog_thickness")
@@ -86,7 +86,7 @@ bool ScriptInterpreter::_executeFe3dGraphicsFunction(const string& functionName,
 		if (_validateListValueAmount(arguments, 0) && _validateListValueTypes(arguments, {}))
 		{
 			auto result = _fe3d.gfx_getFogThickness();
-			returnValues.push_back(ScriptValue(_fe3d, ScriptValueType::DECIMAL));
+			returnValues.push_back(ScriptValue(_fe3d, ScriptValueType::DECIMAL, result));
 		}
 	}
 	else if (functionName == "fe3d:graphics_set_fog_color")
@@ -110,43 +110,7 @@ bool ScriptInterpreter::_executeFe3dGraphicsFunction(const string& functionName,
 		if (_validateListValueAmount(arguments, 0) && _validateListValueTypes(arguments, {}))
 		{
 			auto result = _fe3d.gfx_getFogColor();
-			returnValues.push_back(ScriptValue(_fe3d, ScriptValueType::VEC3));
-		}
-	}
-	else if (functionName == "fe3d:graphics_set_shadow_position")
-	{
-		auto types = { ScriptValueType::DECIMAL, ScriptValueType::DECIMAL, ScriptValueType::DECIMAL };
-
-		if (_validateListValueAmount(arguments, types.size()) && _validateListValueTypes(arguments, types))
-		{
-			_fe3d.gfx_enableShadows(Vec3(arguments[0].getDecimal(), arguments[1].getDecimal(), arguments[2].getDecimal()),
-				_fe3d.gfx_getShadowCenter(), _fe3d.gfx_getShadowSize(), _fe3d.gfx_getShadowReach(), 
-				_fe3d.gfx_getShadowLightness(), _fe3d.gfx_isShadowFollowingCamera(), _fe3d.gfx_isSoftShadowingEnabled());
-			returnValues.push_back(ScriptValue(_fe3d, ScriptValueType::EMPTY));
-		}
-	}
-	else if (functionName == "fe3d:graphics_get_shadow_position")
-	{
-		if (_validateListValueAmount(arguments, 0) && _validateListValueTypes(arguments, {}))
-		{
-			auto result = _fe3d.gfx_getShadowEyePosition();
-			returnValues.push_back(ScriptValue(_fe3d, ScriptValueType::VEC3));
-		}
-	}
-	else if (functionName == "fe3d:graphics_enable_light_mapping")
-	{
-		if (_validateListValueAmount(arguments, 0) && _validateListValueTypes(arguments, {}))
-		{
-			_fe3d.gfx_enableLightMapping();
-			returnValues.push_back(ScriptValue(_fe3d, ScriptValueType::EMPTY));
-		}
-	}
-	else if (functionName == "fe3d:graphics_disable_light_mapping")
-	{
-		if (_validateListValueAmount(arguments, 0) && _validateListValueTypes(arguments, {}))
-		{
-			_fe3d.gfx_disableLightMapping();
-			returnValues.push_back(ScriptValue(_fe3d, ScriptValueType::EMPTY));
+			returnValues.push_back(ScriptValue(_fe3d, ScriptValueType::VEC3, result));
 		}
 	}
 	else if (functionName == "fe3d:graphics_enable_dof")
@@ -231,6 +195,205 @@ bool ScriptInterpreter::_executeFe3dGraphicsFunction(const string& functionName,
 		if (_validateListValueAmount(arguments, 0) && _validateListValueTypes(arguments, {}))
 		{
 			auto result = _fe3d.gfx_getDofBlurDistance();
+			returnValues.push_back(ScriptValue(_fe3d, ScriptValueType::DECIMAL, result));
+		}
+	}
+	else if (functionName == "fe3d:graphics_set_shadow_position")
+	{
+		auto types = { ScriptValueType::DECIMAL, ScriptValueType::DECIMAL, ScriptValueType::DECIMAL };
+
+		if (_validateListValueAmount(arguments, types.size()) && _validateListValueTypes(arguments, types))
+		{
+			_fe3d.gfx_enableShadows(Vec3(arguments[0].getDecimal(), arguments[1].getDecimal(), arguments[2].getDecimal()),
+				_fe3d.gfx_getShadowCenter(), _fe3d.gfx_getShadowSize(), _fe3d.gfx_getShadowReach(),
+				_fe3d.gfx_getShadowLightness(), _fe3d.gfx_isShadowFollowingCamera(), _fe3d.gfx_isSoftShadowingEnabled());
+			returnValues.push_back(ScriptValue(_fe3d, ScriptValueType::EMPTY));
+		}
+	}
+	else if (functionName == "fe3d:graphics_get_shadow_position")
+	{
+		if (_validateListValueAmount(arguments, 0) && _validateListValueTypes(arguments, {}))
+		{
+			auto result = _fe3d.gfx_getShadowEyePosition();
+			returnValues.push_back(ScriptValue(_fe3d, ScriptValueType::VEC3, result));
+		}
+	}
+	else if (functionName == "fe3d:graphics_enable_light_mapping")
+	{
+		if (_validateListValueAmount(arguments, 0) && _validateListValueTypes(arguments, {}))
+		{
+			_fe3d.gfx_enableLightMapping();
+			returnValues.push_back(ScriptValue(_fe3d, ScriptValueType::EMPTY));
+		}
+	}
+	else if (functionName == "fe3d:graphics_disable_light_mapping")
+	{
+		if (_validateListValueAmount(arguments, 0) && _validateListValueTypes(arguments, {}))
+		{
+			_fe3d.gfx_disableLightMapping();
+			returnValues.push_back(ScriptValue(_fe3d, ScriptValueType::EMPTY));
+		}
+	}
+	else if (functionName == "fe3d:graphics_enable_sky_hdr")
+	{
+		if (_validateListValueAmount(arguments, 0) && _validateListValueTypes(arguments, {}))
+		{
+			_fe3d.gfx_enableSkyHDR(_fe3d.gfx_getSkyHdrBrightnessFactor());
+			returnValues.push_back(ScriptValue(_fe3d, ScriptValueType::EMPTY));
+		}
+	}
+	else if (functionName == "fe3d:graphics_disable_sky_hdr")
+	{
+		if (_validateListValueAmount(arguments, 0) && _validateListValueTypes(arguments, {}))
+		{
+			_fe3d.gfx_disableSkyHDR();
+			returnValues.push_back(ScriptValue(_fe3d, ScriptValueType::EMPTY));
+		}
+	}
+	else if (functionName == "fe3d:graphics_set_sky_hdr_brightness")
+	{
+		auto types = { ScriptValueType::DECIMAL };
+
+		if (_validateListValueAmount(arguments, types.size()) && _validateListValueTypes(arguments, types))
+		{
+			bool wasEnabled = _fe3d.gfx_isSkyHdrEnabled();
+			_fe3d.gfx_enableSkyHDR(arguments[0].getDecimal());
+			if (!wasEnabled)
+			{
+				_fe3d.gfx_disableSkyHDR();
+			}
+			returnValues.push_back(ScriptValue(_fe3d, ScriptValueType::EMPTY));
+		}
+	}
+	else if (functionName == "fe3d:graphics_get_sky_hdr_brightness")
+	{
+		if (_validateListValueAmount(arguments, 0) && _validateListValueTypes(arguments, {}))
+		{
+			auto result = _fe3d.gfx_getSkyHdrBrightnessFactor();
+			returnValues.push_back(ScriptValue(_fe3d, ScriptValueType::DECIMAL, result));
+		}
+	}
+	else if (functionName == "fe3d:graphics_enable_motion_blur")
+	{
+		if (_validateListValueAmount(arguments, 0) && _validateListValueTypes(arguments, {}))
+		{
+			_fe3d.gfx_enableMotionBlur(_fe3d.gfx_getMotionBlurStrength());
+			returnValues.push_back(ScriptValue(_fe3d, ScriptValueType::EMPTY));
+		}
+	}
+	else if (functionName == "fe3d:graphics_disable_motion_blur")
+	{
+		if (_validateListValueAmount(arguments, 0) && _validateListValueTypes(arguments, {}))
+		{
+			_fe3d.gfx_disableMotionBlur();
+			returnValues.push_back(ScriptValue(_fe3d, ScriptValueType::EMPTY));
+		}
+	}
+	else if (functionName == "fe3d:graphics_set_motion_blur_strength")
+	{
+		auto types = { ScriptValueType::DECIMAL };
+
+		if (_validateListValueAmount(arguments, types.size()) && _validateListValueTypes(arguments, types))
+		{
+			bool wasEnabled = _fe3d.gfx_isMotionBlurEnabled();
+			_fe3d.gfx_enableMotionBlur(arguments[0].getDecimal());
+			if (!wasEnabled)
+			{
+				_fe3d.gfx_disableMotionBlur();
+			}
+			returnValues.push_back(ScriptValue(_fe3d, ScriptValueType::EMPTY));
+		}
+	}
+	else if (functionName == "fe3d:graphics_get_motion_blur_strength")
+	{
+		if (_validateListValueAmount(arguments, 0) && _validateListValueTypes(arguments, {}))
+		{
+			auto result = _fe3d.gfx_getMotionBlurStrength();
+			returnValues.push_back(ScriptValue(_fe3d, ScriptValueType::DECIMAL, result));
+		}
+	}
+	else if (functionName == "fe3d:graphics_enable_lens_flare")
+	{
+		if (_validateListValueAmount(arguments, 0) && _validateListValueTypes(arguments, {}))
+		{
+			_fe3d.gfx_enableLensFlare(_fe3d.gfx_getLensFlareMapPath(), _fe3d.gfx_getLensFlareIntensity(), _fe3d.gfx_getLensFlareMultiplier());
+			returnValues.push_back(ScriptValue(_fe3d, ScriptValueType::EMPTY));
+		}
+	}
+	else if (functionName == "fe3d:graphics_disable_lens_flare")
+	{
+		if (_validateListValueAmount(arguments, 0) && _validateListValueTypes(arguments, {}))
+		{
+			_fe3d.gfx_disableLensFlare();
+			returnValues.push_back(ScriptValue(_fe3d, ScriptValueType::EMPTY));
+		}
+	}
+	else if (functionName == "fe3d:graphics_set_lens_flare_map")
+	{
+		auto types = { ScriptValueType::STRING };
+
+		if (_validateListValueAmount(arguments, types.size()) && _validateListValueTypes(arguments, types))
+		{
+			bool wasEnabled = _fe3d.gfx_isLensFlareEnabled();
+			_fe3d.gfx_enableLensFlare(arguments[0].getString(), _fe3d.gfx_getLensFlareIntensity(), _fe3d.gfx_getLensFlareMultiplier());
+			if (!wasEnabled)
+			{
+				_fe3d.gfx_disableLensFlare();
+			}
+			returnValues.push_back(ScriptValue(_fe3d, ScriptValueType::EMPTY));
+		}
+	}
+	else if (functionName == "fe3d:graphics_get_lens_flare_map_path")
+	{
+		if (_validateListValueAmount(arguments, 0) && _validateListValueTypes(arguments, {}))
+		{
+			auto result = _fe3d.gfx_getLensFlareMapPath();
+			returnValues.push_back(ScriptValue(_fe3d, ScriptValueType::STRING, result));
+		}
+	}
+	else if (functionName == "fe3d:graphics_set_lens_flare_intensity")
+	{
+		auto types = { ScriptValueType::DECIMAL };
+
+		if (_validateListValueAmount(arguments, types.size()) && _validateListValueTypes(arguments, types))
+		{
+			bool wasEnabled = _fe3d.gfx_isLensFlareEnabled();
+			_fe3d.gfx_enableLensFlare(_fe3d.gfx_getLensFlareMapPath(), arguments[0].getDecimal(), _fe3d.gfx_getLensFlareMultiplier());
+			if (!wasEnabled)
+			{
+				_fe3d.gfx_disableLensFlare();
+			}
+			returnValues.push_back(ScriptValue(_fe3d, ScriptValueType::EMPTY));
+		}
+	}
+	else if (functionName == "fe3d:graphics_get_lens_flare_intensity")
+	{
+		if (_validateListValueAmount(arguments, 0) && _validateListValueTypes(arguments, {}))
+		{
+			auto result = _fe3d.gfx_getLensFlareIntensity();
+			returnValues.push_back(ScriptValue(_fe3d, ScriptValueType::DECIMAL, result));
+		}
+	}
+	else if (functionName == "fe3d:graphics_set_lens_flare_multiplier")
+	{
+		auto types = { ScriptValueType::DECIMAL };
+
+		if (_validateListValueAmount(arguments, types.size()) && _validateListValueTypes(arguments, types))
+		{
+			bool wasEnabled = _fe3d.gfx_isLensFlareEnabled();
+			_fe3d.gfx_enableLensFlare(_fe3d.gfx_getLensFlareMapPath(), _fe3d.gfx_getLensFlareIntensity(), arguments[0].getDecimal());
+			if (!wasEnabled)
+			{
+				_fe3d.gfx_disableLensFlare();
+			}
+			returnValues.push_back(ScriptValue(_fe3d, ScriptValueType::EMPTY));
+		}
+	}
+	else if (functionName == "fe3d:graphics_get_lens_flare_multiplier")
+	{
+		if (_validateListValueAmount(arguments, 0) && _validateListValueTypes(arguments, {}))
+		{
+			auto result = _fe3d.gfx_getLensFlareMultiplier();
 			returnValues.push_back(ScriptValue(_fe3d, ScriptValueType::DECIMAL, result));
 		}
 	}

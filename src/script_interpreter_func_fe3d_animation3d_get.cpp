3ca5f17c2c727d5f08d09e838afd26840e349412
ConsolePeasant92 <hugocornel@yahoo.com>
2021-12-15 23:06:33 +0100

    renamed some CPPs

---
+++ src/script_interpreter_func_fe3d_animation3d_get.cpp
@@ -0,0 +1,135 @@
+#include "script_interpreter.hpp"
+
+using SVT = ScriptValueType;
+
+const bool ScriptInterpreter::_executeFe3dAnimation3dGetter(const string& functionName, vector<ScriptValue>& arguments, vector<ScriptValue>& returnValues)
+{
+	if(functionName == "fe3d:animation3d_is_started")
+	{
+		auto types = {SVT::STRING, SVT::STRING};
+
+		if(_validateArgumentCount(arguments, static_cast<unsigned int>(types.size())) && _validateArgumentTypes(arguments, types))
+		{
+			if(_validateFe3dModel(arguments[0].getString(), false))
+			{
+				auto result = _animation3dEditor.isModelAnimationStarted(arguments[1].getString(), arguments[0].getString());
+				returnValues.push_back(ScriptValue(_fe3d, SVT::BOOLEAN, result));
+			}
+		}
+	}
+	else if(functionName == "fe3d:animation3d_is_playing")
+	{
+		auto types = {SVT::STRING, SVT::STRING};
+
+		if(_validateArgumentCount(arguments, static_cast<unsigned int>(types.size())) && _validateArgumentTypes(arguments, types))
+		{
+			if(_validateFe3dModel(arguments[0].getString(), false))
+			{
+				auto result = _animation3dEditor.isModelAnimationPlaying(arguments[1].getString(), arguments[0].getString());
+				returnValues.push_back(ScriptValue(_fe3d, SVT::BOOLEAN, result));
+			}
+		}
+	}
+	else if(functionName == "fe3d:animation3d_is_paused")
+	{
+		auto types = {SVT::STRING, SVT::STRING};
+
+		if(_validateArgumentCount(arguments, static_cast<unsigned int>(types.size())) && _validateArgumentTypes(arguments, types))
+		{
+			if(_validateFe3dModel(arguments[0].getString(), false))
+			{
+				auto result = _animation3dEditor.isModelAnimationPaused(arguments[1].getString(), arguments[0].getString());
+				returnValues.push_back(ScriptValue(_fe3d, SVT::BOOLEAN, result));
+			}
+		}
+	}
+	else if(functionName == "fe3d:animation3d_is_fading")
+	{
+		auto types = {SVT::STRING, SVT::STRING};
+
+		if(_validateArgumentCount(arguments, static_cast<unsigned int>(types.size())) && _validateArgumentTypes(arguments, types))
+		{
+			if(_validateFe3dModel(arguments[0].getString(), false))
+			{
+				auto result = _animation3dEditor.isModelAnimationFading(arguments[1].getString(), arguments[0].getString());
+				returnValues.push_back(ScriptValue(_fe3d, SVT::BOOLEAN, result));
+			}
+		}
+	}
+	else if(functionName == "fe3d:animation3d_get_speed")
+	{
+		auto types = {SVT::STRING, SVT::STRING};
+
+		if(_validateArgumentCount(arguments, static_cast<unsigned int>(types.size())) && _validateArgumentTypes(arguments, types))
+		{
+			if(_validateFe3dModel(arguments[0].getString(), false))
+			{
+				// Retrieve animation data
+				string errorMessage = "Tried to get animation speed with ID \"" + arguments[1].getString() + "\" on model with ID \"" + arguments[0].getString() + "\": ";
+				auto animationData = _animation3dEditor.getAnimationData(arguments[1].getString(), arguments[0].getString(), errorMessage);
+
+				// Check if animation was found
+				if(animationData != nullptr)
+				{
+					auto result = animationData->getSpeedMultiplier();
+					returnValues.push_back(ScriptValue(_fe3d, SVT::DECIMAL, result));
+				}
+			}
+		}
+	}
+	else if(functionName == "fe3d:animation3d_is_autopaused")
+	{
+		auto types = {SVT::STRING, SVT::STRING};
+
+		if(_validateArgumentCount(arguments, static_cast<unsigned int>(types.size())) && _validateArgumentTypes(arguments, types))
+		{
+			if(_validateFe3dModel(arguments[0].getString(), false))
+			{
+				// Retrieve animation data
+				string errorMessage = "Tried to get animation autopause option with ID \"" + arguments[1].getString() + "\" on model with ID \"" + arguments[0].getString() + "\": ";
+				auto animationData = _animation3dEditor.getAnimationData(arguments[1].getString(), arguments[0].getString(), errorMessage);
+
+				// Check if animation was found
+				if(animationData != nullptr)
+				{
+					auto result = animationData->isAutoPaused();
+					returnValues.push_back(ScriptValue(_fe3d, SVT::BOOLEAN, result));
+				}
+			}
+		}
+	}
+	else if(functionName == "fe3d:animation3d_get_frame_index")
+	{
+		auto types = {SVT::STRING, SVT::STRING};
+
+		if(_validateArgumentCount(arguments, static_cast<unsigned int>(types.size())) && _validateArgumentTypes(arguments, types))
+		{
+			if(_validateFe3dModel(arguments[0].getString(), false))
+			{
+				// Retrieve animation data
+				string errorMessage = "Tried to get animation frame index with ID \"" + arguments[1].getString() + "\" on model with ID \"" + arguments[0].getString() + "\": ";
+				auto animationData = _animation3dEditor.getAnimationData(arguments[1].getString(), arguments[0].getString(), errorMessage);
+
+				// Check if animation was found
+				if(animationData != nullptr)
+				{
+					auto result = animationData->getFrameIndex();
+					returnValues.push_back(ScriptValue(_fe3d, SVT::INTEGER, static_cast<int>(result)));
+				}
+			}
+		}
+	}
+	else
+	{
+		return false;
+	}
+
+	// Cannot execute when server is running
+	if(_fe3d.server_isRunning())
+	{
+		_throwScriptError("cannot access `fe3d:animation3d` functionality as networking server!");
+	}
+
+	// Return
+	return true;
+}
---
+++ src/script_interpreter_func_fe3d_animation3d_set.cpp
@@ -0,0 +1,127 @@
+#include "script_interpreter.hpp"
+
+using SVT = ScriptValueType;
+
+const bool ScriptInterpreter::_executeFe3dAnimation3dSetter(const string& functionName, vector<ScriptValue>& arguments, vector<ScriptValue>& returnValues)
+{
+	if(functionName == "fe3d:animation3d_start")
+	{
+		auto types = {SVT::STRING, SVT::STRING, SVT::INTEGER};
+
+		if(_validateArgumentCount(arguments, static_cast<unsigned int>(types.size())) && _validateArgumentTypes(arguments, types))
+		{
+			if(_validateFe3dModel(arguments[0].getString(), false))
+			{
+				_animation3dEditor.startModelAnimation(arguments[1].getString(), arguments[0].getString(), arguments[2].getInteger());
+				returnValues.push_back(ScriptValue(_fe3d, SVT::EMPTY));
+			}
+		}
+	}
+	else if(functionName == "fe3d:animation3d_pause")
+	{
+		auto types = {SVT::STRING, SVT::STRING};
+
+		if(_validateArgumentCount(arguments, static_cast<unsigned int>(types.size())) && _validateArgumentTypes(arguments, types))
+		{
+			if(_validateFe3dModel(arguments[0].getString(), false))
+			{
+				_animation3dEditor.pauseModelAnimation(arguments[1].getString(), arguments[0].getString());
+				returnValues.push_back(ScriptValue(_fe3d, SVT::EMPTY));
+			}
+		}
+	}
+	else if(functionName == "fe3d:animation3d_resume")
+	{
+		auto types = {SVT::STRING, SVT::STRING};
+
+		if(_validateArgumentCount(arguments, static_cast<unsigned int>(types.size())) && _validateArgumentTypes(arguments, types))
+		{
+			if(_validateFe3dModel(arguments[0].getString(), false))
+			{
+				_animation3dEditor.resumeModelAnimation(arguments[1].getString(), arguments[0].getString());
+				returnValues.push_back(ScriptValue(_fe3d, SVT::EMPTY));
+			}
+		}
+	}
+	else if(functionName == "fe3d:animation3d_fade")
+	{
+		auto types = {SVT::STRING, SVT::STRING, SVT::INTEGER};
+
+		if(_validateArgumentCount(arguments, static_cast<unsigned int>(types.size())) && _validateArgumentTypes(arguments, types))
+		{
+			if(_validateFe3dModel(arguments[0].getString(), false))
+			{
+				_animation3dEditor.fadeModelAnimation(arguments[1].getString(), arguments[0].getString(), arguments[2].getInteger());
+				returnValues.push_back(ScriptValue(_fe3d, SVT::EMPTY));
+			}
+		}
+	}
+	else if(functionName == "fe3d:animation3d_stop")
+	{
+		auto types = {SVT::STRING, SVT::STRING};
+
+		if(_validateArgumentCount(arguments, static_cast<unsigned int>(types.size())) && _validateArgumentTypes(arguments, types))
+		{
+			if(_validateFe3dModel(arguments[0].getString(), false))
+			{
+				_animation3dEditor.stopModelAnimation(arguments[1].getString(), arguments[0].getString());
+				returnValues.push_back(ScriptValue(_fe3d, SVT::EMPTY));
+			}
+		}
+	}
+	else if(functionName == "fe3d:animation3d_set_speed")
+	{
+		auto types = {SVT::STRING, SVT::STRING, SVT::DECIMAL};
+
+		if(_validateArgumentCount(arguments, static_cast<unsigned int>(types.size())) && _validateArgumentTypes(arguments, types))
+		{
+			if(_validateFe3dModel(arguments[0].getString(), false))
+			{
+				// Retrieve animation data
+				string errorMessage = "Tried to set animation speed with ID \"" + arguments[1].getString() + "\" on model with ID \"" + arguments[0].getString() + "\": ";
+				auto animationData = _animation3dEditor.getAnimationData(arguments[1].getString(), arguments[0].getString(), errorMessage);
+
+				// Check if animation was found
+				if(animationData != nullptr)
+				{
+					animationData->setSpeedMultiplier(arguments[2].getDecimal());
+					returnValues.push_back(ScriptValue(_fe3d, SVT::EMPTY));
+				}
+			}
+		}
+	}
+	else if(functionName == "fe3d:animation3d_set_autopaused")
+	{
+		auto types = {SVT::STRING, SVT::STRING, SVT::BOOLEAN};
+
+		if(_validateArgumentCount(arguments, static_cast<unsigned int>(types.size())) && _validateArgumentTypes(arguments, types))
+		{
+			if(_validateFe3dModel(arguments[0].getString(), false))
+			{
+				// Retrieve animation data
+				string errorMessage = "Tried to set animation autopaused option with ID \"" + arguments[1].getString() + "\" on model with ID \"" + arguments[0].getString() + "\": ";
+				auto animationData = _animation3dEditor.getAnimationData(arguments[1].getString(), arguments[0].getString(), errorMessage);
+
+				// Check if animation was found
+				if(animationData != nullptr)
+				{
+					animationData->setAutoPaused(arguments[2].getBoolean());
+					returnValues.push_back(ScriptValue(_fe3d, SVT::EMPTY));
+				}
+			}
+		}
+	}
+	else
+	{
+		return false;
+	}
+
+	// Cannot execute when server is running
+	if(_fe3d.server_isRunning())
+	{
+		_throwScriptError("cannot access `fe3d:animation3d` functionality as networking server!");
+	}
+
+	// Return
+	return true;
+}

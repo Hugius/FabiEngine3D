83245a5d4d3a535c8eb73483b356f524c104e9b7
ConsolePeasant92 <hugocornel@yahoo.com>
2022-05-17 09:39:56 +0200

    working on custom world saving

--- src/custom_world_builder_saving.cpp
+++ src/custom_world_builder_saving.cpp
@@ -19,12 +19,15 @@ const bool CustomWorldBuilder::saveWorldToFile(const string & fileName) const
 
 	auto file = ofstream(filePath);
 
-	for(const auto & skyId : _loadedSkyIds)
+	for(const auto & skyId : _addedSkyIds)
 	{
 		const auto rotation = _fe3d->sky_getRotation(skyId);
 		const auto lightness = _fe3d->sky_getLightness(skyId);
 		const auto color = _fe3d->sky_getColor(skyId);
 		const auto rotationOrder = static_cast<int>(_fe3d->sky_getRotationOrder(skyId));
+		const auto isWireframed = _fe3d->sky_isWireframed(skyId);
+		const auto wireframeColor = _fe3d->sky_getWireframeColor(skyId);
+		const auto isVisible = _fe3d->sky_isVisible(skyId);
 
 		auto cubeMapPaths = _fe3d->sky_getCubeMapPaths(skyId);
 
@@ -63,10 +66,20 @@ const bool CustomWorldBuilder::saveWorldToFile(const string & fileName) const
 			<< color.b
 			<< " "
 			<< rotationOrder
+			<< " "
+			<< isWireframed
+			<< " "
+			<< wireframeColor.r
+			<< " "
+			<< wireframeColor.g
+			<< " "
+			<< wireframeColor.b
+			<< " "
+			<< isVisible
 			<< endl;
 	}
 
-	for(const auto & terrainId : _loadedTerrainIds)
+	for(const auto & terrainId : _addedTerrainIds)
 	{
 		const auto maxHeight = _fe3d->terrain_getMaxHeight(terrainId);
 		const auto textureRepeat = _fe3d->terrain_getTextureRepeat(terrainId);
@@ -78,6 +91,11 @@ const bool CustomWorldBuilder::saveWorldToFile(const string & fileName) const
 		const auto greenTextureRepeat = _fe3d->terrain_getGreenTextureRepeat(terrainId);
 		const auto blueTextureRepeat = _fe3d->terrain_getBlueTextureRepeat(terrainId);
 		const auto isSpecular = _fe3d->terrain_isSpecular(terrainId);
+		const auto isWireframed = _fe3d->terrain_isWireframed(terrainId);
+		const auto wireframeColor = _fe3d->terrain_getWireframeColor(terrainId);
+		const auto minClipPosition = _fe3d->terrain_getMinClipPosition(terrainId);
+		const auto maxClipPosition = _fe3d->terrain_getMaxClipPosition(terrainId);
+		const auto isVisible = _fe3d->terrain_isVisible(terrainId);
 
 		auto heightMapPath = _fe3d->terrain_getHeightMapPath(terrainId);
 		auto diffuseMapPath = _fe3d->terrain_getDiffuseMapPath(terrainId);
@@ -90,16 +108,16 @@ const bool CustomWorldBuilder::saveWorldToFile(const string & fileName) const
 		auto greenNormalMapPath = _fe3d->terrain_getGreenNormalMapPath(terrainId);
 		auto blueNormalMapPath = _fe3d->terrain_getBlueNormalMapPath(terrainId);
 
-		heightMapPath = (heightMapPath.empty() ? "" : heightMapPath.substr(("projects\\" + getCurrentProjectId() + "\\").size()));
-		diffuseMapPath = (diffuseMapPath.empty() ? "" : diffuseMapPath.substr(("projects\\" + getCurrentProjectId() + "\\").size()));
-		normalMapPath = (normalMapPath.empty() ? "" : normalMapPath.substr(("projects\\" + getCurrentProjectId() + "\\").size()));
-		blendMapPath = (blendMapPath.empty() ? "" : blendMapPath.substr(("projects\\" + getCurrentProjectId() + "\\").size()));
-		redDiffuseMapPath = (redDiffuseMapPath.empty() ? "" : redDiffuseMapPath.substr(("projects\\" + getCurrentProjectId() + "\\").size()));
-		greenDiffuseMapPath = (greenDiffuseMapPath.empty() ? "" : greenDiffuseMapPath.substr(("projects\\" + getCurrentProjectId() + "\\").size()));
-		blueDiffuseMapPath = (blueDiffuseMapPath.empty() ? "" : blueDiffuseMapPath.substr(("projects\\" + getCurrentProjectId() + "\\").size()));
-		redNormalMapPath = (redNormalMapPath.empty() ? "" : redNormalMapPath.substr(("projects\\" + getCurrentProjectId() + "\\").size()));
-		greenNormalMapPath = (greenNormalMapPath.empty() ? "" : greenNormalMapPath.substr(("projects\\" + getCurrentProjectId() + "\\").size()));
-		blueNormalMapPath = (blueNormalMapPath.empty() ? "" : blueNormalMapPath.substr(("projects\\" + getCurrentProjectId() + "\\").size()));
+		heightMapPath = (heightMapPath.empty() ? "" : heightMapPath.substr(("projects\\" + _currentProjectId + "\\").size()));
+		diffuseMapPath = (diffuseMapPath.empty() ? "" : diffuseMapPath.substr(("projects\\" + _currentProjectId + "\\").size()));
+		normalMapPath = (normalMapPath.empty() ? "" : normalMapPath.substr(("projects\\" + _currentProjectId + "\\").size()));
+		blendMapPath = (blendMapPath.empty() ? "" : blendMapPath.substr(("projects\\" + _currentProjectId + "\\").size()));
+		redDiffuseMapPath = (redDiffuseMapPath.empty() ? "" : redDiffuseMapPath.substr(("projects\\" + _currentProjectId + "\\").size()));
+		greenDiffuseMapPath = (greenDiffuseMapPath.empty() ? "" : greenDiffuseMapPath.substr(("projects\\" + _currentProjectId + "\\").size()));
+		blueDiffuseMapPath = (blueDiffuseMapPath.empty() ? "" : blueDiffuseMapPath.substr(("projects\\" + _currentProjectId + "\\").size()));
+		redNormalMapPath = (redNormalMapPath.empty() ? "" : redNormalMapPath.substr(("projects\\" + _currentProjectId + "\\").size()));
+		greenNormalMapPath = (greenNormalMapPath.empty() ? "" : greenNormalMapPath.substr(("projects\\" + _currentProjectId + "\\").size()));
+		blueNormalMapPath = (blueNormalMapPath.empty() ? "" : blueNormalMapPath.substr(("projects\\" + _currentProjectId + "\\").size()));
 		heightMapPath = (heightMapPath.empty()) ? "?" : heightMapPath;
 		diffuseMapPath = (diffuseMapPath.empty()) ? "?" : diffuseMapPath;
 		normalMapPath = (normalMapPath.empty()) ? "?" : normalMapPath;
@@ -168,10 +186,32 @@ const bool CustomWorldBuilder::saveWorldToFile(const string & fileName) const
 			<< specularShininess
 			<< " "
 			<< specularIntensity
+			<< " "
+			<< isWireframed
+			<< " "
+			<< wireframeColor.r
+			<< " "
+			<< wireframeColor.g
+			<< " "
+			<< wireframeColor.b
+			<< " "
+			<< minClipPosition.x
+			<< " "
+			<< minClipPosition.y
+			<< " "
+			<< minClipPosition.z
+			<< " "
+			<< maxClipPosition.x
+			<< " "
+			<< maxClipPosition.y
+			<< " "
+			<< maxClipPosition.z
+			<< " "
+			<< isVisible
 			<< endl;
 	}
 
-	for(const auto & waterId : _loadedWaterIds)
+	for(const auto & waterId : _addedWaterIds)
 	{
 		const auto color = _fe3d->water_getColor(waterId);
 		const auto rippleSpeed = _fe3d->water_getRippleSpeed(waterId);
@@ -186,14 +226,20 @@ const bool CustomWorldBuilder::saveWorldToFile(const string & fileName) const
 		const auto isSpecular = _fe3d->water_isSpecular(waterId);
 		const auto isReflective = _fe3d->water_isReflective(waterId);
 		const auto isRefractive = _fe3d->water_isRefractive(waterId);
+		const auto height = _fe3d->water_getHeight(waterId);
+		const auto isWireframed = _fe3d->water_isWireframed(waterId);
+		const auto wireframeColor = _fe3d->water_getWireframeColor(waterId);
+		const auto minClipPosition = _fe3d->water_getMinClipPosition(waterId);
+		const auto maxClipPosition = _fe3d->water_getMaxClipPosition(waterId);
+		const auto isVisible = _fe3d->water_isVisible(waterId);
 
 		auto dudvMapPath = _fe3d->water_getDudvMapPath(waterId);
 		auto normalMapPath = _fe3d->water_getNormalMapPath(waterId);
 		auto heightMapPath = _fe3d->water_getHeightMapPath(waterId);
 
-		dudvMapPath = (dudvMapPath.empty() ? "" : dudvMapPath.substr(("projects\\" + getCurrentProjectId() + "\\").size()));
-		normalMapPath = (normalMapPath.empty() ? "" : normalMapPath.substr(("projects\\" + getCurrentProjectId() + "\\").size()));
-		heightMapPath = (heightMapPath.empty() ? "" : heightMapPath.substr(("projects\\" + getCurrentProjectId() + "\\").size()));
+		dudvMapPath = (dudvMapPath.empty() ? "" : dudvMapPath.substr(("projects\\" + _currentProjectId + "\\").size()));
+		normalMapPath = (normalMapPath.empty() ? "" : normalMapPath.substr(("projects\\" + _currentProjectId + "\\").size()));
+		heightMapPath = (heightMapPath.empty() ? "" : heightMapPath.substr(("projects\\" + _currentProjectId + "\\").size()));
 		dudvMapPath = (dudvMapPath.empty() ? "?" : dudvMapPath);
 		normalMapPath = (normalMapPath.empty() ? "?" : normalMapPath);
 		heightMapPath = (heightMapPath.empty() ? "?" : heightMapPath);
@@ -244,186 +290,31 @@ const bool CustomWorldBuilder::saveWorldToFile(const string & fileName) const
 			<< specularIntensity
 			<< " "
 			<< maxDepth
+			<< " "
+			<< height
+			<< " "
+			<< isWireframed
+			<< " "
+			<< wireframeColor.r
+			<< " "
+			<< wireframeColor.g
+			<< " "
+			<< wireframeColor.b
+			<< " "
+			<< minClipPosition.x
+			<< " "
+			<< minClipPosition.y
+			<< " "
+			<< minClipPosition.z
+			<< " "
+			<< maxClipPosition.x
+			<< " "
+			<< maxClipPosition.y
+			<< " "
+			<< maxClipPosition.z
+			<< " "
+			<< isVisible
 			<< endl;
-	}
-
-	for(const auto & modelId : _loadedModelIds)
-	{
-		const auto partIds = _fe3d->model_getPartIds(modelId);
-		const auto aabbIds = _fe3d->model_getChildAabbIds(modelId);
-		const auto isMultiParted = _fe3d->model_isMultiParted(modelId);
-		const auto modelSize = _fe3d->model_getBaseSize(modelId);
-		const auto levelOfDetailDistance = _fe3d->model_getLevelOfDetailDistance(modelId);
-		const auto rotationOrder = static_cast<int>(_fe3d->model_getRotationOrder(modelId));
-		const auto isShadowed = _fe3d->model_isShadowed(modelId);
-		const auto isReflected = _fe3d->model_isReflected(modelId);
-		const auto isRefracted = _fe3d->model_isRefracted(modelId);
-
-		auto meshPath = _fe3d->model_getMeshPath(modelId);
-		auto levelOfDetailId = _fe3d->model_getLevelOfDetailId(modelId);
-
-		meshPath = (meshPath.empty() ? "" : meshPath.substr(("projects\\" + getCurrentProjectId() + "\\").size()));
-		meshPath = (meshPath.empty()) ? "?" : meshPath;
-		levelOfDetailId = (levelOfDetailId.empty()) ? "?" : levelOfDetailId;
-
-		replace(meshPath.begin(), meshPath.end(), ' ', '?');
-		replace(levelOfDetailId.begin(), levelOfDetailId.end(), ' ', '?');
-
-		file
-			<< "MODEL "
-			<< modelId
-			<< " "
-			<< meshPath
-			<< " "
-			<< modelSize.x
-			<< " "
-			<< modelSize.y
-			<< " "
-			<< modelSize.z
-			<< " "
-			<< levelOfDetailId
-			<< " "
-			<< levelOfDetailDistance
-			<< " "
-			<< rotationOrder
-			<< " "
-			<< isShadowed
-			<< " "
-			<< isReflected
-			<< " "
-			<< isRefracted
-			<< endl;
-
-		for(auto partId : partIds)
-		{
-			const auto isSpecular = _fe3d->model_isSpecular(modelId, partId);
-			const auto specularShininess = _fe3d->model_getSpecularShininess(modelId, partId);
-			const auto specularIntensity = _fe3d->model_getSpecularIntensity(modelId, partId);
-			const auto reflectivity = _fe3d->model_getReflectivity(modelId, partId);
-			const auto refractivity = _fe3d->model_getRefractivity(modelId, partId);
-			const auto lightness = _fe3d->model_getLightness(modelId, partId);
-			const auto color = _fe3d->model_getColor(modelId, partId);
-			const auto textureRepeat = _fe3d->model_getTextureRepeat(modelId, partId);
-			const auto isReflective = _fe3d->model_isReflective(modelId, partId);
-			const auto isRefractive = _fe3d->model_isRefractive(modelId, partId);
-			const auto reflectionType = static_cast<int>(_fe3d->model_getReflectionType(modelId, partId));
-			const auto refractionType = static_cast<int>(_fe3d->model_getRefractionType(modelId, partId));
-			const auto isFaceCulled = _fe3d->model_isFaceCulled(modelId, partId);
-			const auto isBright = _fe3d->model_isBright(modelId, partId);
-			const auto emissionIntensity = _fe3d->model_getEmissionIntensity(modelId, partId);
-			const auto opacity = _fe3d->model_getOpacity(modelId, partId);
-			const auto minTextureAlpha = _fe3d->model_getMinTextureAlpha(modelId, partId);
-
-			auto diffuseMapPath = _fe3d->model_getDiffuseMapPath(modelId, partId);
-			auto emissionMapPath = _fe3d->model_getEmissionMapPath(modelId, partId);
-			auto specularMapPath = _fe3d->model_getSpecularMapPath(modelId, partId);
-			auto reflectionMapPath = _fe3d->model_getReflectionMapPath(modelId, partId);
-			auto refractionMapPath = _fe3d->model_getRefractionMapPath(modelId, partId);
-			auto normalMapPath = _fe3d->model_getNormalMapPath(modelId, partId);
-
-			diffuseMapPath = (diffuseMapPath.empty() ? "" : diffuseMapPath.substr(("projects\\" + getCurrentProjectId() + "\\").size()));
-			emissionMapPath = (emissionMapPath.empty() ? "" : emissionMapPath.substr(("projects\\" + getCurrentProjectId() + "\\").size()));
-			specularMapPath = (specularMapPath.empty() ? "" : specularMapPath.substr(("projects\\" + getCurrentProjectId() + "\\").size()));
-			reflectionMapPath = (reflectionMapPath.empty() ? "" : reflectionMapPath.substr(("projects\\" + getCurrentProjectId() + "\\").size()));
-			refractionMapPath = (refractionMapPath.empty() ? "" : refractionMapPath.substr(("projects\\" + getCurrentProjectId() + "\\").size()));
-			normalMapPath = (normalMapPath.empty() ? "" : normalMapPath.substr(("projects\\" + getCurrentProjectId() + "\\").size()));
-			partId = (partId.empty()) ? "?" : partId;
-			diffuseMapPath = (diffuseMapPath.empty()) ? "?" : diffuseMapPath;
-			emissionMapPath = (emissionMapPath.empty()) ? "?" : emissionMapPath;
-			specularMapPath = (specularMapPath.empty()) ? "?" : specularMapPath;
-			reflectionMapPath = (reflectionMapPath.empty()) ? "?" : reflectionMapPath;
-			refractionMapPath = (refractionMapPath.empty()) ? "?" : refractionMapPath;
-			normalMapPath = (normalMapPath.empty()) ? "?" : normalMapPath;
-
-			replace(diffuseMapPath.begin(), diffuseMapPath.end(), ' ', '?');
-			replace(emissionMapPath.begin(), emissionMapPath.end(), ' ', '?');
-			replace(specularMapPath.begin(), specularMapPath.end(), ' ', '?');
-			replace(reflectionMapPath.begin(), reflectionMapPath.end(), ' ', '?');
-			replace(refractionMapPath.begin(), refractionMapPath.end(), ' ', '?');
-			replace(normalMapPath.begin(), normalMapPath.end(), ' ', '?');
-
-			file
-				<< "PART "
-				<< modelId
-				<< " "
-				<< partId
-				<< " "
-				<< diffuseMapPath
-				<< " "
-				<< emissionMapPath
-				<< " "
-				<< specularMapPath
-				<< " "
-				<< reflectionMapPath
-				<< " "
-				<< refractionMapPath
-				<< " "
-				<< normalMapPath
-				<< " "
-				<< reflectionType
-				<< " "
-				<< refractionType
-				<< " "
-				<< isSpecular
-				<< " "
-				<< isReflective
-				<< " "
-				<< isRefractive
-				<< " "
-				<< specularShininess
-				<< " "
-				<< specularIntensity
-				<< " "
-				<< reflectivity
-				<< " "
-				<< refractivity
-				<< " "
-				<< lightness
-				<< " "
-				<< color.r
-				<< " "
-				<< color.g
-				<< " "
-				<< color.b
-				<< " "
-				<< textureRepeat
-				<< " "
-				<< isFaceCulled
-				<< " "
-				<< isBright
-				<< " "
-				<< emissionIntensity
-				<< " "
-				<< opacity
-				<< " "
-				<< minTextureAlpha
-				<< endl;
-		}
-
-		for(const auto & aabbId : aabbIds)
-		{
-			const auto position = _fe3d->aabb_getLocalPosition(aabbId);
-			const auto size = _fe3d->aabb_getLocalSize(aabbId);
-
-			file
-				<< "AABB "
-				<< modelId
-				<< " "
-				<< aabbId
-				<< " "
-				<< position.x
-				<< " "
-				<< position.y
-				<< " "
-				<< position.z
-				<< " "
-				<< size.x
-				<< " "
-				<< size.y
-				<< " "
-				<< size.z
-				<< endl;
-		}
 	}
 
 	file.close();

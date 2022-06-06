ec93b65559f18db2fd3d7d3b12c155c75b752fa7
ConsolePeasant92 <hugocornel@yahoo.com>
2022-06-06 11:14:03 +0200

    animation3d editor tweaks

--- src/water_editor.hpp
+++ src/water_editor.hpp
@@ -61,8 +61,8 @@ private:
 	static inline constexpr float MIN_CAMERA_DISTANCE = 0.5f;
 	static inline constexpr float COLOR_FACTOR = 255.0f;
 	static inline constexpr float LIGHTNESS_FACTOR = 100.0f;
-	static inline constexpr float RIPPLE_SPEED_FACTOR = 100000.0f;
-	static inline constexpr float WAVE_SPEED_FACTOR = 100000.0f;
+	static inline constexpr float RIPPLE_SPEED_FACTOR = 10000.0f;
+	static inline constexpr float WAVE_SPEED_FACTOR = 10000.0f;
 	static inline constexpr float WAVE_HEIGHT_FACTOR = 100.0f;
 	static inline constexpr float SPECULAR_INTENSITY_FACTOR = 100.0f;
 	static inline constexpr float DISTORTION_SIZE_FACTOR = 100.0f;

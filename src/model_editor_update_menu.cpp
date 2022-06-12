9f15ca8f162361a30c66a62993d577350e3f08dc
ConsolePeasant92 <hugocornel@yahoo.com>
2022-06-12 20:56:14 +0200

    forgot to revert something for testing

--- src/model_editor_update_menu.cpp
+++ src/model_editor_update_menu.cpp
@@ -30,7 +30,7 @@ void ModelEditor::_updateMainMenu()
 		}
 		else if(_fe3d->input_isMousePressed(MouseButtonType::BUTTON_LEFT) && screen->getButton("create")->isHovered())
 		{
-			_gui->getOverlay()->openValueForm("createModel", "Create Model", "", VALUE_FORM_POSITION, VALUE_FORM_SIZE, true, true, true);
+			_gui->getOverlay()->openValueForm("createModel", "Create Model", "", VALUE_FORM_POSITION, VALUE_FORM_SIZE, true, true, false);
 		}
 		else if(_fe3d->input_isMousePressed(MouseButtonType::BUTTON_LEFT) && screen->getButton("edit")->isHovered())
 		{

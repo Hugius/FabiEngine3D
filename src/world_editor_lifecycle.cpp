558e42d8e80890549787486025cde0a96bd93009
ConsolePeasant92 <hugocornel@yahoo.com>
2022-06-15 17:34:20 +0200

    Revert "rollbacked some useless stuff"
    This reverts commit 5defe909e1c11ca1b221c60728823d3624e67f26.

--- src/world_editor_lifecycle.cpp
+++ src/world_editor_lifecycle.cpp
@@ -23,6 +23,7 @@ void WorldEditor::_load()
 	_fe3d->model_setTextureRepeat(GRID_ID, "", GRID_REPEAT);
 	_fe3d->model_setMinAlpha(GRID_ID, "", GRID_MIN_ALPHA);
 	_fe3d->model_setLightness(GRID_ID, "", GRID_LIGHTNESS);
+	_fe3d->model_setShadowed(GRID_ID, false);
 	_fe3d->model_create(LAMP_ID, LAMP_MESH_PATH);
 	_fe3d->model_setBaseSize(LAMP_ID, LAMP_SIZE);
 	_fe3d->model_setShadowed(LAMP_ID, false);

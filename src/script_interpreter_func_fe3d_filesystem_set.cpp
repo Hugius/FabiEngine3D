a86b09cd2821c7e5e6a6f389d00b3dc050005079
ConsolePeasant92 <hugocornel@yahoo.com>
2022-11-30 18:29:49 +0100

    removed some scripting functions

--- src/script_interpreter_func_fe3d_filesystem_set.cpp
+++ src/script_interpreter_func_fe3d_filesystem_set.cpp
@@ -83,11 +83,9 @@ const bool ScriptInterpreter::_executeFe3dFilesystemSetter(const string & functi
 			}
 		}
 	}
-	else if(functionName == "fe3d:file_create")
+	else if(functionName == "fe3d:file_write")
 	{
-		const auto types = {SVT::STRING};
-
-		if(_validateArgumentCount(args, static_cast<int>(types.size())) && _validateArgumentTypes(args, types))
+		if(_validateArgumentCount(args, 2))
 		{
 			if(args[0]->getString().find('/') != string::npos)
 			{
@@ -106,14 +104,26 @@ const bool ScriptInterpreter::_executeFe3dFilesystemSetter(const string & functi
 				const auto directoryPath = (rootPath + (isExported ? "" : ("projects\\" + _currentProjectId + "\\")) + "saves\\");
 				const auto filePath = (directoryPath + args[0]->getString() + ".fe3d");
 
-				if(Tools::isFileExisting(filePath))
-				{
-					_throwRuntimeError("cannot create file");
+				auto file = ofstream(filePath, ios::app);
 
-					return true;
+				if(args[1]->getType() == SVT::STRING)
+				{
+					file << args[1]->getString();
+				}
+				else if(args[1]->getType() == SVT::DECIMAL)
+				{
+					file << to_string(args[1]->getDecimal());
+				}
+				else if(args[1]->getType() == SVT::INTEGER)
+				{
+					file << to_string(args[1]->getInteger());
+				}
+				else if(args[1]->getType() == SVT::BOOLEAN)
+				{
+					file << (args[1]->getBoolean() ? "<true>" : "<false>");
 				}
 
-				auto file = ofstream(filePath);
+				file << endl;
 
 				file.close();
 
@@ -150,99 +160,6 @@ const bool ScriptInterpreter::_executeFe3dFilesystemSetter(const string & functi
 
 					return true;
 				}
-
-				returnValues.push_back(make_shared<ScriptValue>(SVT::EMPTY));
-			}
-		}
-	}
-	else if(functionName == "fe3d:file_write")
-	{
-		if(_validateArgumentCount(args, 2))
-		{
-			if(args[0]->getString().find('/') != string::npos)
-			{
-				_throwRuntimeError("path cannot contain '/'");
-			}
-
-			if(args[0]->getString().find('\\') != string::npos)
-			{
-				_throwRuntimeError("path cannot contain '\\'");
-			}
-
-			if(_validateSavesDirectory())
-			{
-				const auto isExported = Tools::isApplicationExported();
-				const auto rootPath = Tools::getRootDirectoryPath();
-				const auto directoryPath = (rootPath + (isExported ? "" : ("projects\\" + _currentProjectId + "\\")) + "saves\\");
-				const auto filePath = (directoryPath + args[0]->getString() + ".fe3d");
-
-				auto file = ofstream(filePath, ios::app);
-
-				if(!file)
-				{
-					_throwRuntimeError("cannot open file");
-
-					return true;
-				}
-
-				if(args[1]->getType() == SVT::STRING)
-				{
-					file << args[1]->getString();
-				}
-				else if(args[1]->getType() == SVT::DECIMAL)
-				{
-					file << to_string(args[1]->getDecimal());
-				}
-				else if(args[1]->getType() == SVT::INTEGER)
-				{
-					file << to_string(args[1]->getInteger());
-				}
-				else if(args[1]->getType() == SVT::BOOLEAN)
-				{
-					file << (args[1]->getBoolean() ? "<true>" : "<false>");
-				}
-
-				file << endl;
-
-				file.close();
-
-				returnValues.push_back(make_shared<ScriptValue>(SVT::EMPTY));
-			}
-		}
-	}
-	else if(functionName == "fe3d:file_clear")
-	{
-		const auto types = {SVT::STRING};
-
-		if(_validateArgumentCount(args, static_cast<int>(types.size())) && _validateArgumentTypes(args, types))
-		{
-			if(args[0]->getString().find('/') != string::npos)
-			{
-				_throwRuntimeError("path cannot contain '/'");
-			}
-
-			if(args[0]->getString().find('\\') != string::npos)
-			{
-				_throwRuntimeError("path cannot contain '\\'");
-			}
-
-			if(_validateSavesDirectory())
-			{
-				const auto isExported = Tools::isApplicationExported();
-				const auto rootPath = Tools::getRootDirectoryPath();
-				const auto directoryPath = (rootPath + (isExported ? "" : ("projects\\" + _currentProjectId + "\\")) + "saves\\");
-				const auto filePath = (directoryPath + args[0]->getString() + ".fe3d");
-
-				auto file = ofstream(filePath, ios::trunc);
-
-				if(!file)
-				{
-					_throwRuntimeError("cannot open file");
-
-					return true;
-				}
-
-				file.close();
 
 				returnValues.push_back(make_shared<ScriptValue>(SVT::EMPTY));
 			}
